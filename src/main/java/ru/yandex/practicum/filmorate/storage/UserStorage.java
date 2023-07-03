05a51935bec0e0496a9f761194793e7434df7c06
altpa <altpass@gmil.com>
2023-07-03 13:58:56 +0300

    sprint11/transfer

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,13 +1,9 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import lombok.val;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public interface UserStorage {
     Map<Integer, User> getUsers();
@@ -25,3 +21,4 @@ public interface UserStorage {
     List<User> getCommonFriends(int id, int friendId);
 
     User getUserById(int id);
+}
